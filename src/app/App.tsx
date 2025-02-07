877d8f01fe41673c1d1bae7b20811dfdaf5bfd3c
renata-and <renata.androsova@gmail.com>
2025-02-07 12:59:54 +0700

    chore: use baseUrl (absolute paths)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,7 @@
-import { CustomButton } from '../common/Button/CustomButton'
-import { Card } from '../common/Card/Card'
-
 function App() {
   // const [count, setCount] = useState(0)
 
-  return (
-    <div className="font-klein">
-      <CustomButton variant="primary">Name</CustomButton>
-      <CustomButton variant="outlined" padding="lg">
-        Name name
-      </CustomButton>
-      <Card>
-        <p>Hello</p>
-      </Card>
-    </div>
-  )
+  return <div className="font-klein"></div>
 }
 
 export default App
