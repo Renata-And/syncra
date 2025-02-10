a9ef2b012029ca877f8f593c6f55b340e15d7365
Homodeus7 <plum.bus@yahoo.com>
2025-02-10 13:00:08 +0700

    fixed base checkobox styles

--- src/common/components/BaseCheckbox.tsx
+++ src/common/components/BaseCheckbox.tsx
@@ -22,10 +22,13 @@ const BaseCheckbox = ({
     <Checkbox
       checked={checked}
       onChange={onChange}
-      className={`group block size-6 rounded-md border bg-white/10 p-1 ring-1 ring-white/15 ring-inset data-[checked]:bg-white
+      className={`group block size-6 border border-card-border rounded-md bg-white p-1 ring-inset data-[checked]:bg-primary data-[checked]:dark:bg-primary-dark data-[checked]:border-none
       ${boxClassName}`}
     >
-      <Check className={`hidden size-4 fill-black group-data-[checked]:block ${iconClassName}`} />
+      <Check
+        color="white"
+        className={`hidden size-4 fill-black group-data-[checked]:block ${iconClassName}`}
+      />
     </Checkbox>
     <Label>{label}</Label>
     {error && <span className="text-xs text-error absolute right-0 -bottom-4">{error}</span>}
